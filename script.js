905c79804288c5ecaff09533603fcfa831efceea
Sergius.Moogo <sergius.moogo@equitybank.co.ke>
2024-10-04 12:01:27 +0300

    Add initial structure for Friends of Warthog

---
+++ script.js
@@ -0,0 +1,5 @@
+// Basic code for future functionality or dynamic features on the website
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Welcome to Friends of Warthog!");
+  // Additional functionality can be added here
+});
